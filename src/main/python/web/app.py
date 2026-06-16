129590b02170bc2adec7c3d21f77e305363dbb1f
ChristianSaFl <christian.sana@ucsp.edu.pe>
2026-06-16 00:17:10 -0500

    Corregir exposición de red y recursos externos sin integridad. Fix #2

--- src/main/python/web/app.py
+++ src/main/python/web/app.py
@@ -729,4 +729,4 @@ def delete_user(uid):
 
 if __name__ == "__main__":
     initialize_database()
-    app.run(host="0.0.0.0", port=8083, debug=True)
+    app.run(host="127.0.0.1", port=8083, debug=True)
